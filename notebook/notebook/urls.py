4b13a0c30c7d0866ec624ab741311d43122525e8
Jenny6199 <Jenny6199@yandex.ru>
2022-02-23 19:41:40 +0300

    add routers in notebook/urls.py

--- notebook/notebook/urls.py
+++ notebook/notebook/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from user.views import PersonModelViewSet
+
+router = DefaultRouter
+router.register = ('user', PersonModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
+    path('api/', include(router.urls)),
 ]
--- notebook/user/views.py
+++ notebook/user/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Person
 from .serializers import PersonModelSerialazer
 
+
 class PersonModelViewSet(ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonModelSerialazer
