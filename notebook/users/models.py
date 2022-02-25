f7dedf1e2b5633ed8e124cbb2d9965d163710cee
Jenny6199 <Jenny6199@yandex.ru>
2022-02-25 05:41:07 +0300

    well done! finish task

--- notebook/users/models.py
+++ notebook/users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     surname = models.CharField(max_length=64, help_text='Фамилия', verbose_name='Фамилия', blank=False)
     email = models.EmailField(unique=True, help_text='E-mail', verbose_name='E-mail')
     birthday = models.DateField(help_text='Дата рождения', verbose_name='Дата рождения')
-    is_active = models.BooleanField(help_text='Статус', verbose_name='Статус')
+    is_active = models.BooleanField(help_text='Статус', verbose_name='Статус', default=True)
     created_at = models.DateTimeField(auto_now_add=True, help_text='Зарегистрирован', verbose_name='Зарегистрирован')
     updated_at = models.DateTimeField(auto_now=True, help_text='Последнее редактирование', verbose_name='Последнее редактирование')
     
