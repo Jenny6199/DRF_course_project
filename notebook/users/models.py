b10baaea337513d154480c025ee878085a3ff811
Jenny6199 <Jenny6199@yandex.ru>
2022-02-25 04:16:50 +0300

    change fields in models.py

--- notebook/users/models.py
+++ notebook/users/models.py
@@ -3,20 +3,19 @@ from random import choices
 from django.db import models
 from uuid import uuid4
 from django.contrib.auth.models import AbstractUser
-from django.forms import BooleanField, CharField, DateField, DateTimeField, EmailField, UUIDField
 
 
 class User(AbstractUser):
     """Модель пользователя"""
-    uuid = UUIDField(primary_key=True, default=uuid4, null=False, unique=True)
-    first_name = CharField(max_length=64, help_text='Имя', verbose_name='Имя', blank=False)
-    parent_name = CharField(max_length=64, help_text='Отчество', verbose_name='Отчество', blank=True)
-    surname = CharField(max_length=64, help_text='Фамилия', verbose_name='Фамилия', blank=False)
-    email = EmailField(unique=True, help_text='E-mail', verbose_name='E-mail')
-    birthday = DateField(help_text='Дата рождения', verbose_name='Дата рождения')
-    is_active = BooleanField(help_text='Статус', verbose_name='Статус')
-    created_at = DateTimeField(auto_now_add=True, help_text='Зарегистрирован', verbose_name='Зарегистрирован')
-    updated_at = DateTimeField(auto_now=True, help_text='Последнее редактирование', verbose_name='Последнее редактирование')
+    id = models.UUIDField(primary_key=True, null=False, unique=True, default=uuid4)
+    first_name = models.CharField(max_length=64, help_text='Имя', verbose_name='Имя', blank=False)
+    parent_name = models.CharField(max_length=64, help_text='Отчество', verbose_name='Отчество', blank=True)
+    surname = models.CharField(max_length=64, help_text='Фамилия', verbose_name='Фамилия', blank=False)
+    email = models.EmailField(unique=True, help_text='E-mail', verbose_name='E-mail')
+    birthday = models.DateField(help_text='Дата рождения', verbose_name='Дата рождения')
+    is_active = models.BooleanField(help_text='Статус', verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, help_text='Зарегистрирован', verbose_name='Зарегистрирован')
+    updated_at = models.DateTimeField(auto_now=True, help_text='Последнее редактирование', verbose_name='Последнее редактирование')
     
     USER_ROLE = (
         ('A', 'Administrator'),
@@ -24,7 +23,7 @@ class User(AbstractUser):
         ('D', 'Developer'),
     )
 
-    role = CharField(max_length=1, choices=USER_ROLE, blank=False, help_text='Тип учетной записи')
+    role = models.CharField(max_length=1, choices=USER_ROLE, blank=False, help_text='Тип учетной записи')
 
     
     def __str__(self):
