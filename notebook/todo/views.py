669ebac5b64263730d5e993e8d8a31e1c528780d
Jenny6199 <Jenny6199@yandex.ru>
2022-03-09 17:39:35 +0300

    add class ProjectAPIVIew in todo/views.py

--- notebook/todo/views.py
+++ notebook/todo/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
+# Для использования APIView
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 
 class ProjectModelViewSet(ModelViewSet):
@@ -14,3 +18,13 @@ class ToDoModelViewSet(ModelViewSet):
     """Класс представлений для модели ToDo"""
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+
+
+class ProjectAPIVIew(APIView):
+    """Класс обработки запросов к Project"""
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, format=None):
+        projects = Project.objects.all()
+        serializer = ProjectModelSerializer(projects,many=True)
+        return Response(serializer.data)
