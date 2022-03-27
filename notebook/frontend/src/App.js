a2360a235ab0ae2fd21afb1e1b90978499e35f0f
Jenny6199 <Jenny6199@yandex.ru>
2022-03-27 12:18:11 +0300

    transfer table users from App.js to user.js

--- notebook/frontend/src/App.js
+++ notebook/frontend/src/App.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import './App.css';
-import UserList from './components/user';
+import Users from './components/user';
 import MainMenu from './components/menu';
 import ToDo from './components/todo';
 import PageFooter from './components/footer';
 import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'users': []
-    }
-  }
-
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users/?limit=10')
-    .then(response => {
-      const users = response.data.results
-        this.setState(
-          {
-            'users': users
-          }
-        )
-    }).catch(error => console.log(error))
-  }
-
   render ()  {
     return (
       <div>
         <MainMenu />
-        <UserList users={this.state.users} />
+        <Users/>
         <ToDo />
         <PageFooter />
       </div>
--- notebook/frontend/src/components/user.js
+++ notebook/frontend/src/components/user.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const UserItem = ({user}) => {
     return (
@@ -42,4 +43,33 @@ const UserList = ({users}) => {
     )
 }
 
-export default UserList;
+class Users extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        'users': []
+      }
+    }
+  
+    componentDidMount() {
+      axios.get('http://127.0.0.1:8000/api/users/?limit=10')
+      .then(response => {
+        const users = response.data.results
+          this.setState(
+            {
+              'users': users
+            }
+          )
+      }).catch(error => console.log(error))
+    }
+  
+    render ()  {
+      return (
+        <div>
+          <UserList users={this.state.users} />
+        </div>
+      )
+    }
+  }
+
+export default Users;
