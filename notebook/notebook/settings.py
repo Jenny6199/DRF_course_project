d23fd2d047182523c3447929c33dd37504b7fe83
Jenny6199 <Jenny6199@yandex.ru>
2022-04-01 15:45:13 +0300

    add DEFAULT_AUTHENTICATION_CLASSES in settings.py

--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -153,5 +153,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
