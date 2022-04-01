1a1b4f6a93a11e4412e066aea131aa9641bfb3f8
Jenny6199 <Jenny6199@yandex.ru>
2022-04-01 17:58:40 +0300

    add path for api-token-auth in urls.py

--- notebook/notebook/urls.py
+++ notebook/notebook/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 from todo.views import ProjectSpecialViewSet,ToDoSpecialViewSet
 from users.views import UserSpecialViewSet
 
@@ -14,4 +15,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    # Путь для получения токена пользователя
+    path('api-token-auth/', views.obtain_auth_token)
 ]
