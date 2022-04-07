75c6f6d1604c95e2a98b160d188e90c08b629827
Jenny6199 <Jenny6199@yandex.ru>
2022-04-08 01:34:30 +0300

    replace get_token and load_data in menu.js

--- notebook/frontend/src/App.js
+++ notebook/frontend/src/App.js
@@ -2,54 +2,10 @@ import React from 'react';
 import './App.css';
 import MainMenu from './components/menu';
 import PageFooter from './components/footer';
-import axios from 'axios';
+
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'users': [],
-      'projects': [],
-      'todo': []
-    }
-  }
-
-  get_path() {
-    const api = 'http://127.0.0.1:8000/api/'
-    this.setState = {
-      api_path: api
-    }
-  }
-
-  load_data() {
-    const api_path = this.api_path
-
-    axios.get(api_path + 'users/').then(response => {
-      this.setState({users: response.data})
-    }).catch(error => console.log(error))
-
-    axios.get(api_path + 'projects/').then(response => {
-      this.setState({projects: response.data})
-    }).catch(error => console.log(error))
-
-    axios.get(api_path + 'todos/').then(response => {
-      this.setState({todo: response.data})
-    }).catch(error => console.log(error))
-  }
-
-  get_token(username, password) {
-
-    const api_path = this.api_path
-
-    axios.post(api_path + 'api-token-auth/', {username: username, password: password}).then(response => {
-      console.log(response.data)
-    }).catch(error => alert('Ошибка ввода логина или пароля!'))
-  }
-
-  componentDidMount() {
-    this.load_data()
-  }
 
   render ()  {
     return (
--- notebook/frontend/src/components/menu.js
+++ notebook/frontend/src/components/menu.js
@@ -4,6 +4,7 @@ import Projects from './project';
 import Users from './user';
 import ToDo from './todo';
 import LoginForm from './Auth';
+import axios from 'axios';
 
 const NotFound404 =({ location }) => {
     return (
@@ -13,39 +14,79 @@ const NotFound404 =({ location }) => {
     )
   }
 
-const MainMenu = () => {
-    return (
-        <div>
-            <h3>Главное меню</h3>
-            <BrowserRouter>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/users/'>Пользователи</Link>
-              </li>
-              <li>
-                <Link to='/projects/'>Проекты</Link>
-              </li>
-              <li>
-                <Link to='/todo/'>Заметки</Link>
-              </li>
-              <li>
-                  <Link to='/login'>Авторизация</Link>
-              </li>
-            </ul>
-          </nav>
-          <hr></hr>
-          <Switch>
-            <Route exact path='/users' component={() => <Users />} />
-            <Route exact path='/projects' component={() => <Projects />} />
-            <Route exact path='/todo' component={() => <ToDo />} />
-            <Route exoct path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)} />} />
-            <Route component={NotFound404} />
-            <Redirect from='/' to='/users' />
-          </Switch>
-        </BrowserRouter>
-        </div>
-    )
+class MainMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': [],
+            'projects': [],
+            'todo': [],
+        }
+    }
+
+    get_path(){
+        return 'http://127.0.0.1:8000/'
+    } 
+
+    load_data() {
+        axios.get(this.get_path() + 'api/users/').then(response => {
+            this.setState({users: response.data})
+        }).catch(error => console.log(error))
+
+        axios.get(this.get_path() + 'api/projects/').then(response => {
+            this.setState({projects: response.data})
+        }).catch(error => console.log(error))
+
+        axios.get(this.get_path() + 'api/todo/').then(response => {
+            this.setState({todo: response.data})
+        }).catch(error => console.log(error))
+    }
+    
+    get_token(username, password) {
+        axios.post(this.get_path() + 'api-token-auth/', {username: username, password: password}).then(response => {
+            console.log(response.data)
+        }).catch(error => alert('Ошибка ввода логина или пароля!'))
+    }
+
+    componentDidMount() {
+        this.load_data()
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Главное меню</h3>
+                <BrowserRouter>
+                <nav>
+                <ul>
+                    <li>
+                    <Link to='/users/'>Пользователи</Link>
+                    </li>
+                    <li>
+                    <Link to='/projects/'>Проекты</Link>
+                    </li>
+                    <li>
+                    <Link to='/todo/'>Заметки</Link>
+                    </li>
+                    <li>
+                        <Link to='/login'>Авторизация</Link>
+                    </li>
+                </ul>
+                </nav>
+                <hr></hr>
+                <Switch>
+                <Route exact path='/users' component={() => <Users />} />
+                <Route exact path='/projects' component={() => <Projects />} />
+                <Route exact path='/todo' component={() => <ToDo />} />
+                <Route exoct path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)} />} />
+                <Route component={NotFound404} />
+                <Redirect from='/' to='/users' />
+                </Switch>
+            </BrowserRouter>
+            </div>
+        )
+    }
+
 }
 
 export default MainMenu;
