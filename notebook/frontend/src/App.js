ae14d3737304229727cd5f1b4648c4414caca847
Jenny6199 <Jenny6199@yandex.ru>
2022-04-08 00:06:09 +0300

    out path from api in different const

--- notebook/frontend/src/App.js
+++ notebook/frontend/src/App.js
@@ -2,49 +2,49 @@ import React from 'react';
 import './App.css';
 import MainMenu from './components/menu';
 import PageFooter from './components/footer';
-/*
-import Users from './components/user';
-import Projects from './components/project';
-import ToDo from './components/todo';
-import {BrowserRouter, Route, Switch, Redirect, Link} from 'react-router-dom';
-*/
 import axios from 'axios';
-
-/*
-const NotFound404 = ({location}) => {
-  return (
-    <div>
-      <h3>Страница по адресу '{location.pathname}' не найдена</h3>
-    </div>
-  )
-}
-*/
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.stat = {
+    this.state = {
       'users': [],
       'projects': [],
       'todo': []
     }
   }
 
-  load_data() {
-    const api_path = 'http://127.0.0.1:8000/api/'
+  get_path() {
+    const api = 'http://127.0.0.1:8000/api/'
+    this.setState = {
+      api_path: api
+    }
+  }
 
-    axios.get(api_path + 'users').then(response => {
+  load_data() {
+    const api_path = this.api_path
+
+    axios.get(api_path + 'users/').then(response => {
       this.setState({users: response.data})
     }).catch(error => console.log(error))
 
-    axios.get(api_path + 'projects').then(response => {
+    axios.get(api_path + 'projects/').then(response => {
       this.setState({projects: response.data})
     }).catch(error => console.log(error))
 
-    axios.get(api_path + 'todos').then(response => {
+    axios.get(api_path + 'todos/').then(response => {
       this.setState({todo: response.data})
     }).catch(error => console.log(error))
+  }
+
+  get_token(username, password) {
+
+    const api_path = this.api_path
+    
+    axios.post(api_path + 'api-token-auth/', {username: username, password: password}).then(response => {
+      console.log(response.data)
+    }).catch(error => alert('Ошибка ввода логина или пароля!'))
   }
 
   componentDidMount() {
@@ -57,32 +57,7 @@ class App extends React.Component {
         <MainMenu />
         <hr></hr>
         <PageFooter />
-      </div>
-/*    <div className="App">
-        <BrowserRouter>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/users/'>Пользователи</Link>
-              </li>
-              <li>
-                <Link to='/projects/'>Проекты</Link>
-              </li>
-              <li>
-                <Link to='/todo/'>Заметки</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path='/users' component={() => <Users />} />
-            <Route exact path='/projects' component={() => <Projects />} />
-            <Route exact path='/todo' component={() => <ToDo />} />
-            <Route component={NotFound404} />
-            <Redirect from='/' to='/users' />
-          </Switch>
-        </BrowserRouter>
-      </div>
-*/      
+      </div>      
     )
   }
 }
