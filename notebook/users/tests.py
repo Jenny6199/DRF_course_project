b641583ab4ba8fdefd8515341d94aec8a483524f
Jenny6199 <Jenny6199@yandex.ru>
2022-04-11 18:16:59 +0300

    start users/test.py

--- notebook/users/tests.py
+++ notebook/users/tests.py
@@ -1,3 +1,14 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth import models
+from .views import UserSpecialViewSet
+from .models import User
 
-# Create your tests here.
+class TestUserSpecialViewSet(TestCase):
+    """
+    Класс для тестирования контроллера модели User
+    """
+    pass
