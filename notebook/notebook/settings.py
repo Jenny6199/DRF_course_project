0809c3623e566c743217762a4f5af1a49a399d32
Jenny6199 <Jenny6199@yandex.ru>
2022-04-15 18:23:07 +0300

    AcceptHeadersVersioning done

--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -163,9 +163,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+# Варианты версионирования API    
 #    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 #    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
+#    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
 }
 
 
