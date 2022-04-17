e40e030914a0f90e40224b098f828ff1cbc40919
Jenny6199 <Jenny6199@yandex.ru>
2022-04-17 22:59:45 +0300

    add settings for graphql in settings.py

--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -210,3 +210,8 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+# Settings for graphql
+GRAPHENE = {
+    'SCHEMA': 'notebook.schema.schema'
+}
