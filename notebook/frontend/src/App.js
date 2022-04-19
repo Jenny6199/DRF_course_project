ed0c0e7b914bb6199e4a3e7a138e3d2108d5766a
Jenny6199 <Jenny6199@yandex.ru>
2022-04-20 00:35:26 +0300

    add deleteToDO in App.js

--- notebook/frontend/src/App.js
+++ notebook/frontend/src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './App.css';
 import MainMenu from './components/menu';
 import PageFooter from './components/footer';
+import axios from 'axios';
 
 
 class App extends React.Component {
+
+  deleteToDo(id) {
+    const headers = this.get_headers()
+    axios.delete('http://127.0.0.1:8000/api/todo/${id}', {headers, headers})
+      .then(response => {
+        this.setState({todo: this.state.todo.filter((item) => item.id !== id)})
+    }).catch(error => console.log(error))
+  }
 
   render ()  {
     return (
