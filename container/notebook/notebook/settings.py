570aa5bec072a7b640712499f675f2b52f12f6d3
Maksim_Sapunov <jenny6199@yandex.ru>
2022-04-30 21:55:38 +0300

    change ALLOWED_HOSTS in settings.py

--- container/notebook/notebook/settings.py
+++ container/notebook/notebook/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = dark_path
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['194.58.11.96']
 
 
 # Application definition
